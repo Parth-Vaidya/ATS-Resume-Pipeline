00b787d51c92d5b79fc80af40734394020ebf1ba
Parth-Vaidya <hellopvaidya@gmail.com>
2026-09-14 01:31:34 +0530

    Database is connected to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,16 @@
 import app from "./app.js";
+import pool from "./config/dataabase.js";
 
 const PORT= 5000;
 
 app.listen(PORT , ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 })
+
+pool.query("SELECT NOW()",(err,result)=>{
+    if(err){
+        console.error("Database connection failed:", err.message);
+    }else{
+        console.log("Database connected:", result.rows[0]);
+    }
+});
